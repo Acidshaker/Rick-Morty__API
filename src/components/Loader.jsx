345c994dea073dda4c82f825c3887f2c5d935694
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-12-03 20:49:29 -0600

    second commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='loader__content'>
-      <img src="./src/assets/portal-rick-and-morty.gif" alt="Portal image Rick and Morty" />
-      <img src="./src/assets/loading.png" alt="Loading image" className='loading' />
+      <img src="./public/img/portal-rick-and-morty.gif" alt="Portal image Rick and Morty" />
+      <img src="./public/img/loading.png" alt="Loading image" className='loading' />
     </div>
   )
 }
