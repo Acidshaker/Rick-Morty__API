d7da598384a6f6d527db6f572e4c8f3d87e4a9bd
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-12-03 20:56:40 -0600

    second commit

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorMessage = () => {
   return (
     <div className='error__container'>
-      <img className='img__error' src="./public/morty-error.png" alt="Image error" />
+      <img className='img__error' src="./src/assets/img/morty-error.png" alt="Image error" />
       <h2 className='error__msg'>Sorry not found</h2>
     </div>
   )
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='loader__content'>
-      <img src="./public/portal-rick-and-morty.gif" alt="Portal image Rick and Morty" />
-      <img src="./public/loading.png" alt="Loading image" className='loading' />
+      <img src="./src/assets/img/portal-rick-and-morty.gif" alt="Portal image Rick and Morty" />
+      <img src="./src/assets/img/loading.png" alt="Loading image" className='loading' />
     </div>
   )
 }
