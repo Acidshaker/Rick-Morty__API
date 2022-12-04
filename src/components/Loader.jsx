b2d4c953b77a7696bc3ca6dcbb70a48b2da80cf1
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-12-04 00:45:32 -0600

    responsive 1

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,9 +4,11 @@ const Loader = () => {
   return (
     <div className='loader__content'>
       <img src="https://i.postimg.cc/k4rwhf5B/portal-rick-and-morty.gif" alt="Portal image Rick and Morty" />
-      <img src="https://i.postimg.cc/g0LNDfmC/loading.png" alt="Loading image" className='loading' />
+      <img src="https://i.postimg.cc/Pqc5wVMV/loading1.png" alt="Loading image" className='loading' />
     </div>
   )
 }
+
+// https://i.postimg.cc/g0LNDfmC/loading.png
 
 export default Loader
