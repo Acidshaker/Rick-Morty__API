388e978427b98e13422617be42635f0f88c8ec2b
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-12-06 02:18:11 -0600

    final

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -4,8 +4,7 @@ const ErrorMessage = () => {
   return (
     <div className='error__container'>
       <img className='img__error' src="https://i.postimg.cc/6qXjTYsD/morty-error.png" alt=" Image error" />
-      <h2 className='error__msg'>Sorry dimension not found</h2>
-      <p></p>
+      <h3 className='error__msg'>Sorry dimension not found</h3>
     </div>
   )
 }
